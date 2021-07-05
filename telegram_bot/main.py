b6673b70c34d91e16175a8dc65ff3ef80411efac
Claudio795 <claudiocurto@hotmail.it>
2021-07-05 13:22:17 +0200

    Aggiunti comandi al bot per la richiesta e l'aggiornamento della posizione dell'utente

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,51 +1,92 @@
+from collections import UserList
 import logging
 import os
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
-    Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+    conversationhandler
 )
 from paho.mqtt.client import Client
-
-from telegram.ext.callbackcontext import CallbackContext
 from telegram.message import Message
 from telegram.update import Update
 
 from config import TOKEN
 
+LOCATION = range(4)
 
 # elenco funzioni del bot
 def start(update, context):
     #all'interno di questa funzione instaureremo la connessione al broker
-    msg = (
-        "Bot avviato"
+    
+    reply_keyboard = [["/annulla"]]
+    update.message.reply_text('Bot avviato, inviami la tua posizione per cominciare',
+    reply_markup = ReplyKeyboardMarkup(
+        reply_keyboard, one_time_keyboard=True
+        ),
     )
-    update.message.reply_text(msg)
+    return LOCATION
 
-# Test_1: ritorno della posizione GPS
-def get_location(update, context):
-    print("messaggio ricevuto")
-    print(update.message.from_user)
-    if update.edited_message:
-        message = update.edited_message
-    else:
-        message = update.message
+#funzione per richiedere un aggiornamento della posizione tramite comando 
+def location(update, context):
+    user = update.message.from_user["first_name"]
+    user_location = update.message.location
+    update.message.reply_text("Nuova posizione ricevuta")
+    print(f"Coordinate di {user}: LAT: {user_location.latitude}, LONG: {user_location.longitude}")
+    return ConversationHandler.END
 
-    lat = message.location.latitude
-    lng = message.location.longitude
-    #update.message.reply_text(f"latitudine: {lat}, longitudine: {lng}")
-    print(f"latitudine: {lat}, longitudine: {lng}")
+# funzione per aggiornare la posizione
+def update_location(update, context):
+    user = update.message.from_user["first_name"]
+    reply_keyboard = [["/annulla"]]
+    update.message.reply_text(f'Ok {user}, inviami la tua nuova posizione',
+    reply_markup = ReplyKeyboardMarkup(
+        reply_keyboard, one_time_keyboard=True
+        ),
+    )
+    return LOCATION
+
+# funzione per annullare un comando 
+def cancel(update, context):
+    #user = update.message.from_user
+    update.message.reply_text("Operazione annullata.")
+    return ConversationHandler.END
+
+
     
 
 
-# ----------------------------------------
+# funzione main ----------------------------------------
 def main():
     upd = Updater(TOKEN, use_context=True)
     disp = upd.dispatcher
 
-    disp.add_handler(CommandHandler("start", start))
+    #elenco di handler per i comandi del bot
+    start_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states= {
+            LOCATION: [MessageHandler(Filters.location, location)]
+        },
+        fallbacks=[CommandHandler("annulla", cancel)]
+        )
 
-    disp.add_handler(MessageHandler(Filters.location, get_location))
+    location_handler = ConversationHandler(
+        entry_points=[CommandHandler("aggiorna_posizione", update_location)],
+        states= {
+            LOCATION: [MessageHandler(Filters.location, location)]
+        },
+        fallbacks=[CommandHandler("annulla", cancel)]
+    )
 
+    disp.add_handler(start_handler)
+    disp.add_handler(location_handler)
+
+    # avvio del bot, resta in esecuzione fino ad quando non
+    #riceve un CTRL+C, SIGTERM O SIGABRT
     upd.start_polling()
     upd.idle()
 
