7da29192e0a79ed53311be7cad5028c14c8de356
manlio <manpuglisi94@gmail.com>
2021-07-05 13:09:08 +0200

    [linkedin] ottengo i link delle offerte di lavoro

--- publishers/testLinkedin.js
+++ publishers/testLinkedin.js
@@ -5,14 +5,14 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto('https://www.linkedin.com/jobs', { waitUntil: 'load'});
+    await page.goto('https://www.linkedin.com/jobs', { waitUntil: 'load' });
 
     // const jobLocationForm = await page.$$eval('input', inputs => {
     //     console.log(inputs.length)
     // });
 
     // inseriamo il tipo di qualifica o azienda:
-    await page.type('.dismissable-input__input','');
+    await page.type('.dismissable-input__input', '');
     await page.keyboard.press('Tab');
     // inseriamo la zona:
     await page.type('.dismissable-input__input', 'Catania')
@@ -23,7 +23,16 @@ const puppeteer = require('puppeteer');
     console.log('New Page URL: ', page.url())
 
     // TODO: inserire elaborazione delle row per restituire al publisher i lavori
+    let jobs = await page.$$(`[data-tracking-control-name=public_jobs_jserp-result_search-card]`);
+    let jobsHandles = await Promise.all(
+        jobs.map(divHandler => divHandler.getProperty('href'))
+    )
+    let links = await Promise.all(
+        jobsHandles.map(handle => handle.jsonValue())
+    )
+
+    console.log(links[0], links[1], links[2]);
 
     // END
-    await browser.close();
+    // await browser.close();
 })();
