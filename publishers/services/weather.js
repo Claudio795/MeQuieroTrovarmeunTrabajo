8f221833f7cc106e80b80f7ef253bd15ba11e396
manlio <manpuglisi94@gmail.com>
2021-07-15 03:23:38 +0200

    aggiunta funzione per ottenere informazioni sul meteo tramite openweathermap.org

---
+++ publishers/services/weather.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const key = fs.readFileSync("../openWeatherAPI.key");
+const unirest = require("../node_modules/unirest");
+
+const language = 'it';
+
+const getWeather = city => {
+    return new Promise((res, rej) => {
+        let cityWeather = unirest.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=${language}`);
+        res(cityWeather)
+    }).then(res => {
+        return [
+            (res.body.main.temp - 273), // la temperatura la si ottiene in Kelvin
+            res.body.weather[0].description // weather è un array con un solo elemento
+        ];
+    })
+}
+
+module.exports = getWeather;
+
+/** Esempio
+getWeather("Sant'Agata Li Battiati").then(res => {
+    let info = res;
+    console.log(info);
+});
+*/
